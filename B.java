4000925b17fd059d01e43d9d9bbd001f010e496c
vmezhevikin <ve.mezhevikin@gmail.com>
2016-03-17 15:23:57 +0200

    changed B

--- B.java
+++ B.java
@@ -2,6 +2,16 @@ public class B
 {
 	private int id;
 
+	public B()
+	{
+		this(25);
+	}
+
+	public B(int id)
+	{
+		this.id = id;
+	}
+
 	public void set(int id)
 	{
 		this.id = id;
---
+++ B.java~
@@ -0,0 +1,14 @@
+public class B
+{
+	private int id;
+
+	public void set(int id)
+	{
+		this.id = id;
+	}
+
+	public int get()
+	{
+		return id;
+	}
+}
